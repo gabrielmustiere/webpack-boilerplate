1e2b17ee757d59125e0aa850b5abcaa2b48af38c
gabrielmustiere <mustiere.gabriel@gmail.fr>
2018-10-22 22:44:13 +0200

    play with promise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-import style from "./main.css"
+let shouldResolve = true
+
+let maPromesse = (shouldResolve) => {
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      setTimeout(() => {
+        resolve('executé')
+      }, 1000)
+    } else {
+      reject('echec')
+    }
+  })
+}
+
+maPromesse(shouldResolve).then(retourPromesse => {
+  console.log("1");
+  console.log('Then 1 : ', retourPromesse)
+
+  return maPromesse(shouldResolve).then(retourPromesseImbrique => {
+    setTimeout(() => {
+      console.log("2")
+      console.log("Then imbriqué : ", retourPromesseImbrique)
+    }, 2000)
+    return "Then imbrique : " + retourPromesseImbrique
+  }).then(() => {
+    return maPromesse(shouldResolve).then(retourPromesseImbrique => {
+      setTimeout(() => {
+        console.log("3")
+        console.log("Then imbriqué : ", retourPromesseImbrique)
+        return "Then imbrique 2 : " + retourPromesseImbrique
+      }, 1000)
+    })
+  })
+
+})
+.then(retourPremierThen => {
+  console.log("5")
+  console.log("Then 2 : ", retourPremierThen)
+
+  ///
+
+  return "Then 2 : " + retourPremierThen
+})
+.then((retourSecondThen) => {
+  console.log("6")
+  console.log("Then 3 : ", retourSecondThen)
+
+  ///
+
+  return "Then 3 : " + retourSecondThen
+})
+.catch((errorPromesse) => {
+  console.log(errorPromesse)
+})
